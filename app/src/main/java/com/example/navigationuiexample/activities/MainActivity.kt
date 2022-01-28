deebdad6c7df96530cc1259fab4cfce4631d25a9
Lenovo <gunabdulkadir21@gmail.com>
2022-01-28 14:58:17 +0300

    - initial commit

--- app/src/main/java/com/example/navigationuiexample/activities/MainActivity.kt
+++ app/src/main/java/com/example/navigationuiexample/activities/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                 }
             true
         }
+
        /* binding.mainToolbar.setNavigationOnClickListener {
             navController.navigate(HomeFragmentDirections.actionHomeFragmentToCategories())
 
